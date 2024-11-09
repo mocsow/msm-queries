03a6b621b8149e5d25698f289cc71a76bb9a0482
mocsow <mcsow@alumni.chicagobooth.edu>
2024-11-09 05:57:17 +0000

    created director details page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
 
   get("/directors", {:controller => "director", :action => "index"})
+
+  get("/directors/:the_id", {:controller => "director", :action => "show"})
 end
